d765162920fa9f597303d02da9354b9752458994
unknown <ian.green@uk.ibm.com>
2016-02-04 15:34:26 +0000

    build fixed; needs npm link for ts plugin

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,62 +5,48 @@ var babel   = require("gulp-babel");
 var watch   = require("gulp-watch");
 var gutil   = require("gulp-util");
 var gulp    = require("gulp");
-var path    = require("path");
 var debug   = require("gulp-debug");
+var rename   = require("gulp-rename");
 
-var scripts = "./packages/*/src/**/*.js";
-var dest = "build";
+//var sourceRoot = "d:/dev/ng60-2/com.ibm.rdm.web/resources";
+//var scripts = sourceRoot + "/artifact/AbstractResourceArtifactWidget.js";
+var sourceRoot = "./src";
+var scripts = sourceRoot + "/**/*.js";
+//var dest = "d:/temp/ngjs/dng/TypeScriptHTMLApp1/resources";
+//var scripts = "./src/**/*.js";
+var dest = "./build";
 
-var srcEx, libFragment;
-console.log(path.win32);
-
-if (path.win32 === path) {
-    console.log("win32");
-  srcEx = /(packages\\[^\\]+)\\src\\/;
-  libFragment = "$1\\lib\\";
-} else {
-    console.log("not win32");
-  srcEx = new RegExp("(packages/[^/]+)/src/");
-  libFragment = "$1/lib/";
-}
-
+var cwd = process.cwd();
 gulp.task("default", ["build"]);
 
 gulp.task("build", function () {
-  return gulp.src(scripts)
-        .pipe(debug({title: "sources"}))
-    .pipe(plumber({
-      errorHandler: function (err) {
-          debug("ccccc");
-        gutil.log(err.stack);
-      }
-    }))
-        .pipe(debug({title: "sources2"}))
-
-    .pipe(through.obj(function (file, enc, callback) {
-      file._path = file.path;
-      file.path = file.path.replace(srcEx, libFragment);
-        gutil.log("info", "'" + file.path + "'...");
-      callback(null, file);
-    }))
-        .pipe(debug({title:"here"}))
-/*    .pipe(newer(dest))
-        .pipe(debug({title:"here2"}))
-*/
-    .pipe(through.obj(function (file, enc, callback) {
-        debug({title:"here"})
-        gutil.log("Compiling", "'" + file._path + "'...");
-        callback(null, file);
-    }))
-        .pipe(debug({title:"to babel"}))
-
-    .pipe(babel())
-        .pipe(debug({title:"to dest"}, false))
-    .pipe(gulp.dest(dest));
+    return gulp.src(scripts, {base: sourceRoot})
+//        .pipe(debug({title: "sources"}))
+        .pipe(watch(scripts))
+        .pipe(plumber({
+            errorHandler: function (err) {
+                gutil.log(err.stack);
+            }
+        }))
+        .pipe(newer({dest:dest, map: function(p) {
+            return p.replace(".js", ".ts");
+        }}))
+        .pipe(debug({title: "Compiling..."}))
+        .pipe(babel({
+            plugins: [
+                [cwd + "/node_modules/babel-plugin-transform-typescript-prepare", {
+                    literal_only:true,
+                    to_ts:true }
+                ]
+            ],
+            retainLines:true,
+            compact:false}))
+        .pipe(rename({extname: ".ts"}))
+        .pipe(gulp.dest(dest));
 });
 
 gulp.task("watch", ["build"], function (callback) {
-  watch(scripts, function () {
-    gulp.start("build");
-  });
+    watch(scripts, function () {
+        gulp.start("build");
+    });
 });
